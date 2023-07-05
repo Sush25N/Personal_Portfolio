d1135ba74191b4703d035e2ad7a5c42d99e661d0
Sush25N <sushgabhane123@gmail.com>
2023-07-05 23:44:34 +0530

    some routes

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import "./about.css"
-
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import {FaAward} from "react-icons/fa";
+import {AiFillFolderOpen} from "react-icons/ai";
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1.5+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <AiFillFolderOpen className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+ Completed Projects</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./nav.css";
 import {AiOutlineHome} from "react-icons/ai";
 import {AiOutlineUser} from "react-icons/ai";
@@ -6,21 +6,34 @@ import {GiSpellBook} from "react-icons/gi";
 import {TbMessages} from "react-icons/tb";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>
-      <a href="#">
+    <nav>
+      <a href="#" className={activeNav === "#" ? "active" : ""}>
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <GiSpellBook />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <TbMessages />
       </a>
-    </div>
+    </nav>
   );
 };
 
