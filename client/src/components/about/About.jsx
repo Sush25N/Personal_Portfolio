d352f0a55968e4bca87df99d29e7c80f042d95b4
Sush25N <sushgabhane123@gmail.com>
2023-07-06 21:14:21 +0530

    about page

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -30,6 +30,16 @@ const About = () => {
               <small>5+ Completed Projects</small>
             </article>
           </div>
+          <p>
+            👋 Hi there! I'm a passionate and determined beginner MERN stack
+            developer, eager to create amazing web applications and contribute
+            to the world of software development. As a beginner, I believe in
+            the power of continuous learning. I'm always seeking out new
+            opportunities to expand my knowledge and improve my skills.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
--- client/src/components/experience/Experience.jsx
+++ client/src/components/experience/Experience.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import "./experience.css";
+import {BiSolidMessageSquareCheck} from "react-icons/bi";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        {/* Frontend Starts */}
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>HTML5</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>CSS3</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>BootStrap</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>React JS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+
+        {/* Backend Starts */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>Node JS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+
+            <article className="experience__details">
+              <BiSolidMessageSquareCheck />
+              <h4>Mongo DB</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
