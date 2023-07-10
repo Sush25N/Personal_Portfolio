a9fa044844951d6a86804116b369281af9c45f94
Sush25N <sushgabhane123@gmail.com>
2023-07-10 21:09:57 +0530

    succeed testing header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "./header.css";
-// import CTA from "./CTA";
-// import ME from "../../assets/me.png";
-// // import ME from "../../assets/IMG_20230225_213638.jpg"
-// import HeaderSocials from "./HeaderSocials";
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+// import ME from "../../assets/IMG_20230225_213638.jpg"
+import HeaderSocials from "./HeaderSocials";
 
-// const Header = () => {
-//   return (
-//     <header>
-//       <div className="container header_container">
-//         <h5>Hello I'm</h5>
-//         <h1>Sushrut Gabhane</h1>
-//         <h5 className="text-light">MERN Stack Developer</h5>
-//         <CTA />
-//         <HeaderSocials />
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Sushrut Gabhane</h1>
+        <h5 className="text-light">MERN Stack Developer</h5>
+        <CTA />
+        <HeaderSocials />
 
-//         <div className="me">
-//           <img src={ME} alt="me" />
-//         </div>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
 
-//         <a href="#contact" className="scroll_down">
-//           Scroll Down
-//         </a>
-//       </div>
-//     </header>
-//   );
-// };
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
-// export default Header;
+export default Header;
