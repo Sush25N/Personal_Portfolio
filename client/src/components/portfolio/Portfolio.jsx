2b778b862737d31f7e0801279685f8ee6d3391f1
Sush25N <sushgabhane123@gmail.com>
2023-07-17 00:16:15 +0530

    finished project

--- client/src/components/portfolio/Portfolio.jsx
+++ client/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg"
-import IMG2 from "../../assets/portfolio2.jpg"
-import IMG3 from "../../assets/portfolio3.jpg"
-import IMG4 from "../../assets/portfolio4.jpg"
-import IMG5 from "../../assets/portfolio5.png"
-import IMG6 from "../../assets/portfolio6.jpg"
+import IMG1 from "../../assets/EasyJOBS.jpg"
+import IMG2 from "../../assets/dynamic_quotes.jpg"
+import IMG3 from "../../assets/weather_application.avif"
+import IMG4 from "../../assets/Calculator.jpg"
+import IMG5 from "../../assets/image-slider.jpg"
+import IMG6 from "../../assets/food_order_app.jpg"
 
 
 const Portfolio = () => {
